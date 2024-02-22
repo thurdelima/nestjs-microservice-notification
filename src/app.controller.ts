2861c65b437aa0846c901fd9e420379c5cadd6c6
Arthur <thurdelima@gmail.com>
2024-02-22 00:25:44 -0300

    feat: first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @EventPattern('task_notification')
+  taskNotification(data: any) {
+    console.log("recebendo msg");
+    console.log(data);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  //const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {port: 30002, host: '127.0.0.1'},
+    }
+  );
+  await app.listen();
+
+
+}
+bootstrap();
